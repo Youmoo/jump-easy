1efe6c4a39cd28edead930e7fa07a46b93eca32c
youmoo <youmoolee@gmail.com>
2015-09-14 13:55:12 +0800

    search baidu yun using google

--- js/baidu-yun.js
+++ js/baidu-yun.js
@@ -1,6 +1,6 @@
 (function () {
     function getword(info, tab) {
-        var url = "http://www.baidu.com/#wd={wd} site:pan.baidu.com";
+        var url = "http://www.google.com/#q={wd} site:pan.baidu.com";
         var wd = encodeURIComponent(info.selectionText);
         chrome.tabs.create({
             url: url.replace("{wd}", wd)
